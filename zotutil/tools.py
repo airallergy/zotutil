b1543c574f1b3cda0ff2d49a552955d682d31fa5
airallergy <49818327+airallergy@users.noreply.github.com>
2020-06-21 21:33:20 +0100

    Resolve #4 nested empty directories removal

--- zotutil/tools.py
+++ zotutil/tools.py
@@ -2,15 +2,23 @@ from pathlib import Path
 import sys
 
 
-def remove_empty_directories(root_directory):
+def remove_empty_directories(root_directory, parent_directory=None):
     root_directory = Path(root_directory)
+    if parent_directory:
+        parent_directory = Path(parent_directory)
+    else:
+        parent_directory = root_directory
     if sys.platform == "darwin":
-        remove_ds_store(root_directory)
-    for directory in tuple(item for item in root_directory.iterdir() if item.is_dir()):
+        remove_ds_store(parent_directory)
+    for directory in tuple(
+        item for item in parent_directory.iterdir() if item.is_dir()
+    ):
         if not tuple(directory.glob("*")):
             directory.rmdir()
+            if root_directory <= parent_directory.parent:
+                remove_empty_directories(root_directory, parent_directory.parent)
         else:
-            remove_empty_directories(directory)
+            remove_empty_directories(root_directory, directory)
 
 
 def remove_ds_store(root_directory):
